c636bc4924d153166f9a2af7f5ed28859b2327f5
Ali Raza <160298055+alirazapoonja@users.noreply.github.com>
2024-08-16 19:10:47 +0500

    song composing in 1 days question 2
    song composing in 1 days question 2

---
+++ code_1.ts
@@ -0,0 +1,5 @@
+let famous_movie: string = "Kaalia"; // // This saves the movie name
+let message: string =
+  '"\n Jahan teri ye coding hai,\n Meri jaa mujhe error khabar hai,\n Bach na saka koi error kitne,\n Lambe hain mere error itne,\n Dekh idhar yaar Ameen Sir Aidhar hai.';
+console.log(`${famous_movie} composing song, ${message}`); // song composing coding show in result
+
