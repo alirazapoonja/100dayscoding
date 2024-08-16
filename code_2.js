4c8af711d1659a1cc56dd5c1e52d76bd6073f2b3
Ali Raza <160298055+alirazapoonja@users.noreply.github.com>
2024-08-16 19:22:50 +0500

    movie name in lower case and upper case
    movie name in lower case and upper case

---
+++ code_2.js
@@ -0,0 +1,5 @@
+let moviename = 'Kaalia';
+console.log(moviename.toLowerCase()); // Shows the name in all small letters
+console.log(moviename.toUpperCase()); // Shows the name in ALL BIG LETTERS
+console.log(moviename.charAt(0).toUpperCase() + moviename.slice(1).toLowerCase()); // Shows the name with the First Letter Big
+export {};
